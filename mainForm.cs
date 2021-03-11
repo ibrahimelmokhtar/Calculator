a6f63728e17a4f674db3be34dc28b90b8db8ae70
Ibrahim El-Mokhtar <ibrahimelmokhtar@gmail.com>
2021-03-11 16:41:14 +0200

    Add buttons' functionality

--- mainForm.cs
+++ mainForm.cs
@@ -22,99 +22,158 @@ namespace Calculator
             InitializeComponent();
         }
 
+        private void displayNumber(char charNumber)
+        {
+            if (txtOperation.Text != "")
+            {
+                txtNumber2.Text += charNumber;
+            }
+            else
+            {
+                txtResult.Text += charNumber;
+            }
+        }
+
+        private void applyOperation()
+        {
+            if (txtNumber2.Text != "")
+            {
+                firstNumber = double.Parse(txtResult.Text);
+                secondNumber = double.Parse(txtNumber2.Text);
+                switch (txtOperation.Text)
+                {
+                    case "+":
+                        result = firstNumber + secondNumber;
+                        break;
+                    case "-":
+                        result = firstNumber - secondNumber;
+                        break;
+                    case "*":
+                        result = firstNumber * secondNumber;
+                        break;
+                    case "/":
+                        result = firstNumber / secondNumber;
+                        break;
+                    case "%":
+                        result = firstNumber % secondNumber;
+                        break;
+                }
+                txtResult.Text = result.ToString();
+                txtNumber2.Clear();
+            }
+        }
+
         private void btn0_Click(object sender, EventArgs e)
         {
-            txtResult.Text += '0';
+            displayNumber('0');
         }
 
         private void btn1_Click(object sender, EventArgs e)
         {
-            txtResult.Text += '1';
+            displayNumber('1');
         }
 
         private void btn2_Click(object sender, EventArgs e)
         {
-            txtResult.Text += '2';
+            displayNumber('2');
         }
 
         private void btn3_Click(object sender, EventArgs e)
         {
-            txtResult.Text += '3';
+            displayNumber('3');
         }
 
         private void btn4_Click(object sender, EventArgs e)
         {
-            txtResult.Text += '4';
+            displayNumber('4');
         }
 
         private void btn5_Click(object sender, EventArgs e)
         {
-            txtResult.Text += '5';
+            displayNumber('5');
         }
 
         private void btn6_Click(object sender, EventArgs e)
         {
-            txtResult.Text += '6';
+            displayNumber('6');
         }
 
         private void btn7_Click(object sender, EventArgs e)
         {
-            txtResult.Text += '7';
+            displayNumber('7');
         }
 
         private void btn8_Click(object sender, EventArgs e)
         {
-            txtResult.Text += '8';
+            displayNumber('8');
         }
 
         private void btn9_Click(object sender, EventArgs e)
         {
-            txtResult.Text += '9';
+            displayNumber('9');
         }
 
         private void btnPlus_Click(object sender, EventArgs e)
         {
-            txtResult.Text += "+";
+            applyOperation();
+            txtOperation.Text = "+";
         }
 
         private void btnMinus_Click(object sender, EventArgs e)
         {
-            txtResult.Text += '-';
+            applyOperation();
+            txtOperation.Text = "-";
         }
 
         private void btnMultiply_Click(object sender, EventArgs e)
         {
-            txtResult.Text += '*';
+            applyOperation();
+            txtOperation.Text = "*";
         }
 
         private void btnDivide_Click(object sender, EventArgs e)
         {
-            txtResult.Text += '/';
+            applyOperation();
+            txtOperation.Text = "/";
         }
 
         private void btnModulus_Click(object sender, EventArgs e)
         {
-            txtResult.Text += '%';
+            applyOperation();
+            txtOperation.Text = "%";
         }
 
         private void btnEqual_Click(object sender, EventArgs e)
         {
-            txtResult.Text = result.ToString();
+            applyOperation();
+            txtOperation.Clear();
+            txtNumber2.Clear();
         }
 
         private void btnDot_Click(object sender, EventArgs e)
         {
-            txtResult.Text += '.';
+            displayNumber('.');
         }
 
         private void btnClear_Click(object sender, EventArgs e)
         {
             txtResult.Clear();
+            txtOperation.Clear();
+            txtNumber2.Clear();
         }
 
         private void btnDelete_Click(object sender, EventArgs e)
         {
-            if (txtResult.Text != "")
+            if (txtOperation.Text != "" && txtNumber2.Text != "")
+            {
+                int lastIndex = txtNumber2.Text.Length - 1;
+                txtNumber2.Text = txtNumber2.Text.Remove(lastIndex);
+            }
+            else if (txtNumber2.Text == "")
+            {
+                txtOperation.Clear();
+            }
+            else
             {
                 int lastIndex = txtResult.Text.Length - 1;
                 txtResult.Text = txtResult.Text.Remove(lastIndex);
